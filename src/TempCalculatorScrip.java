432c5704463145874d052dfd37085152e49d2dbc
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 12:35:51 -0500

    Second Commit

---
+++ src/TempCalculatorScrip.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; //Import to activate scanner function
+
+public class TempCalculatorScrip {
+    public static void main(String[] args) {
+
+        //Project init
+        String tempCelsius = ""; //Declaring initial variable
+
+
+        Scanner in = new Scanner(System.in); // Scanner function for the variables
+        System.out.print("Give a temperature in Celsius ");
+        C=(F-32)*5/9
+
+    }
+}
