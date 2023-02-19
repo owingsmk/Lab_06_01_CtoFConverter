91b700bc60d8b08cbea17ecd50ef5cb85a5cc453
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 12:21:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Project init
+        String tempCelsius = ""; //Declare 
+
+
+    }
+}
