b1628aab63fc7fd197ca531076b581b5f684a47f
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 13:02:10 -0500

    Final Commit

--- src/TempCalculatorScrip.java
+++ src/TempCalculatorScrip.java
@@ -4,12 +4,14 @@ public class TempCalculatorScrip {
     public static void main(String[] args) {
 
         //Project init
-        String tempCelsius = ""; //Declaring initial variable
 
+        Scanner celius = new Scanner(System.in); // Scanner function for the variables
+        System.out.print("Enter a temperature in Celsius "); //Output for the user to inter information
+        double C = celius.nextDouble(); //Variable for celcius for the scanner
+        double fahrenheitFinal = ((C*9/5)+32); // the formula to convert Celius to Fahrenheit
 
-        Scanner in = new Scanner(System.in); // Scanner function for the variables
-        System.out.print("Give a temperature in Celsius ");
-        C=(F-32)*5/9
+        System.out.println("Your temperature in Fahrenheit is " + fahrenheitFinal); //The final output of the program
+        }
+
 
     }
-}
